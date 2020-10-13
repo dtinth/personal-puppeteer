c095d7fe20a4abab65667d6c49a26cab410263d6
dtinth on Cloud9 <dtinth@spacet.me>
2020-10-13 15:46:19 +0000

    Make signing utility support w/h

--- sign.js
+++ sign.js
@@ -9,16 +9,30 @@ require('yargs')
       url: {
         type: 'string',
       },
+      width: {
+        type: 'number',
+        alias: 'w',
+        default: 1280,
+      },
+      height: {
+        type: 'height',
+        alias: 'h',
+        default: 720,
+      },
     },
     async (args) => {
       const key = require('fs').readFileSync(
         require.resolve('./private/jwtRS256.key')
       )
-      const jwt = require('jsonwebtoken').sign({ url: args.url }, key, {
-        algorithm: 'RS256',
-        noTimestamp: true,
-        issuer: 'dtinth',
-      })
+      const jwt = require('jsonwebtoken').sign(
+        { url: args.url, width: args.width, height: args.height },
+        key,
+        {
+          algorithm: 'RS256',
+          noTimestamp: true,
+          issuer: 'dtinth',
+        }
+      )
       console.log(jwt)
     }
   )
